9c13862281c171005ea28bf17355beb6031c287c
Robert <robcampbell@ucmo.edu>
2019-03-11 11:04:24 -0500

    Remove commented code

--- main.go
+++ main.go
@@ -13,22 +13,7 @@ type Vampire struct {
 
 type Vampires []Vampire
 
-// func returnVampires() {
-//
-//   vampire := Vampires{
-//     Vampire{Name: "Vlad Tepes", Country: "Romania", Died: "December 1476"},
-//     Vampire{Name: "Elizabeth Bathory", Country: "Hungary", Died: "August 1614"},
-//   }
-//
-//   v := json.NewEncoder(os.Stdout).Encode(vampire)
-//
-//   return v
-//
-// }
-
 func main() {
-
-  // returnVampires()
 
   vampire := Vampires{
     Vampire{Name: "Vlad Tepes", Country: "Romania", Died: "December 1476"},
